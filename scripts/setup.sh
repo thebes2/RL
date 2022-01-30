6b9407502401ddf20dbe8e13f8f282e8b6f188a4
George Chen <ggchen2k2@gmail.com>
2022-01-30 15:51:23 -0500

    Compatibility with python < 3.8

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install python3
+sudo apt-get install python3.8
 sudo apt-get install python3-venv
 sudo apt-get install python3-pip
 
+pip3 install virtualenv
+
 mkdir -p venv
-python3 -m venv ./venv
+python3 -m virtualenv --python=`which python3.8` ./venv
 
 source ./venv/bin/activate
 pip install wheel
